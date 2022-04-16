27a45f3d68892f0a0b244d2539b1f5e2fd0d7ae6
Wayne Smart <smudgeness@gmail.com>
2022-04-16 15:51:22 +0200

    it validates at least

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, presence: true
+  validates :comment, length: { minimum: 6 }
+  validates :list_id, uniqueness: { scope: :movie_id,
+    message: "should be unique" }
+end
